66f160aab8b57134ed2d89e7bfbf175b36fe9da6
Ahmed Elgendy <ahmedellgendy@outlook.com>
2026-07-26 13:54:48 +0300

    Display inventory quantities using large and small unit

--- Salesync.SalesRep.Pwa/Models/Responses/SalesRepInventoryDto.cs
+++ Salesync.SalesRep.Pwa/Models/Responses/SalesRepInventoryDto.cs
@@ -3,19 +3,17 @@
     public class SalesRepInventoryDto
     {
         public int Id { get; set; }
-
         public int SalesRepId { get; set; }
-
         public string? SalesRepName { get; set; }
-
-        public int ProductId { get; set; }
-
+        public int ProductId { get; set; } 
         public string? ProductName { get; set; }
-
         public string? ItemCode { get; set; }
 
+        // محفوظة بالوحدة الصغرى
         public int Quantity { get; set; }
-
+        public string SmallUnit { get; set; } = "قطعة";
+        public string LargeUnit { get; set; } = "كرتونة";
+        public int UnitsPerLargeUnit { get; set; } = 1;
         public DateTime LastUpdatedAt { get; set; }
     }
 }
